0d5e72fe30c9611dca00a6d297ba264f023c461a
RajeevKrSah <rajeevjnvkne@gmail.com>
2024-12-23 15:22:16 +0530

    Add database connection with mongodb

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,6 @@
-import express from "express"
-
 import 'dotenv/config'
-
+import express from "express"
+import connectDB from './db/index.js'
 const app = express()
 
 const PORT = process.env.PORT || 4000
@@ -9,6 +8,16 @@ const PORT = process.env.PORT || 4000
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 
+// Database connection 
+connectDB()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server is running at port ${PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connectin failed!!!",err);
+})
 
 
 app.get('/', (req,res)=>{
@@ -17,10 +26,5 @@ app.get('/', (req,res)=>{
 
 app.get("/api/v1",(req,res)=>{
     res.send("This is from Luxora")
-})
-
-app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`);
-    
 })
 
