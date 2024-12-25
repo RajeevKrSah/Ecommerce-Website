fd515052f7262bc43a351fd3d5c5857eaadfae84
RajeevKrSah <rajeevjnvkne@gmail.com>
2024-12-25 19:39:53 +0530

    Add User Model

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,21 @@
 import 'dotenv/config'
 import express from "express"
 import connectDB from './db/index.js'
-const app = express()
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
+const app = express()
+ 
 const PORT = process.env.PORT || 4000
 
+// Middlewares
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(cookieParser())
 
 // Database connection 
 connectDB()
@@ -24,7 +33,4 @@ app.get('/', (req,res)=>{
     res.send("Hello Luxora")
 })
 
-app.get("/api/v1",(req,res)=>{
-    res.send("This is from Luxora")
-})
 
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,22 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Navbar from "../components/Navbar";
-import axios from "axios";
+
 const Home = () => {
-  const [data, setData] = useState("");
-  useEffect(() => {
-    axios
-      .get("/api/v1/")
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  });
+
   return (
     <>
       <Navbar />
-      <h1>{data}</h1>
     </>
   );
 };
